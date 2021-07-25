94fcb7c53239e00ca65a9f3c75b14c172be72bc5
Kseniia Prytkova <prytkovakseniia@gmail.com>
2021-07-25 17:03:54 +0300

    add LinkCard component

--- client/src/pages/DetailPage.js
+++ client/src/pages/DetailPage.js
@@ -3,6 +3,7 @@ import {useParams} from 'react-router-dom'
 import {useHttp} from '../hooks/http.hook'
 import {AuthContext} from '../context/AuthContext'
 import {Loader} from '../components/Loader'
+import {LinkCard} from '../components/LinkCard'
 
 export const DetailPage = () => {
     // to get the token 
@@ -33,7 +34,7 @@ export const DetailPage = () => {
 
     return (
         <>
-            { !loading && link && <LinkCard /> }
+            { !loading && link && <LinkCard link={link}/> }
         </>
     )
 }
